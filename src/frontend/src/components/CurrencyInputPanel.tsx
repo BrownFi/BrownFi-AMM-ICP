22e8bff67a7b3a875f93f097d4e409fc06f1cc0d
Truong Ma Phi <trmaphi@gmail.com>
2024-03-22 00:17:48 +0700

    wip: setup swap ui

---
+++ src/frontend/src/components/CurrencyInputPanel.tsx
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+const InputPanel = styled.div<{ hideInput?: boolean }>`
+  ${({ theme }) => theme.flexColumnNoWrap}
+  position: relative;
+  background-color: ${({ theme, hideInput }) => (hideInput ? theme.primary1 : theme.primary1)};
+  z-index: 1;
+  width: ${({ hideInput }) => (hideInput ? '100%' : 'initial')};
+  padding: 16px 20px;
+`;
+
+export default function CurrencyInputPanel() {
+    return (
+        <InputPanel>
+        </InputPanel>
+    );
+}
