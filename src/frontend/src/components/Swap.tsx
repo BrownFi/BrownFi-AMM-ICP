6364146b0bf42c720f945165bf1cd8b3ed0ce632
Truong Ma Phi <trmaphi@gmail.com>
2024-03-22 00:03:21 +0700

    fix: swap UI

--- src/frontend/src/components/Swap.tsx
+++ src/frontend/src/components/Swap.tsx
@@ -2,108 +2,41 @@ import { createReactor } from "@ic-reactor/react";
 import { idlFactory } from "../../../declarations/token1";
 import { styled } from "styled-components";
 import { colors, theme } from "../theme";
-
-// import CurrencyInputPanel, {
-//   ShortcutAmount,
-// } from "../../components/CurrencyInputPanel";
-
-// const PageWrapper = styled(AutoColumn)`
-//   width: 100%;
-// `;
-
-// export default function Swap() {
-//   const { login, identity } = useInternetIdentity();
-
-//   return (
-//     <>
-//       <AppBody>
-//         <SwapHeader />
-//         <Wrapper id="swap-page">
-//           <AutoColumn gap={"md"}>
-//             <div style={{ display: "relative" }}>
-//               <CurrencyInputPanel
-//                 label={
-//                   independentField === Field.OUTPUT && !showWrap
-//                     ? "From (at most)"
-//                     : "From"
-//                 }
-//                 value={formattedAmounts[Field.INPUT]}
-//                 showMaxButton={showMaxButton}
-//                 showShortcutButtons={true}
-//                 currency={currencies[Field.INPUT]}
-//                 onUserInput={handleTypeInput}
-//                 onMax={handleMaxInput}
-//                 onShortcutAmount={handleHalfInput}
-//                 fiatValue={fiatValueInput ?? undefined}
-//                 onCurrencySelect={handleInputSelect}
-//                 otherCurrency={currencies[Field.OUTPUT]}
-//                 showCommonBases={true}
-//                 id="swap-currency-input"
-//               />
-
-//               <CurrencyInputPanel
-//                 value={formattedAmounts[Field.OUTPUT]}
-//                 onUserInput={handleTypeOutput}
-//                 label={
-//                   independentField === Field.INPUT && !showWrap
-//                     ? "To (at least)"
-//                     : "To"
-//                 }
-//                 showMaxButton={false}
-//                 hideBalance={false}
-//                 fiatValue={fiatValueOutput ?? undefined}
-//                 priceImpact={priceImpact}
-//                 currency={currencies[Field.OUTPUT]}
-//                 onCurrencySelect={handleOutputSelect}
-//                 otherCurrency={currencies[Field.INPUT]}
-//                 showCommonBases={true}
-//                 id="swap-currency-output"
-//               />
-//             </div>
-//           </AutoColumn>
-//         </Wrapper>
-//       </AppBody>
-//     </>
-//   );
-// }
+import AppBody from "../AppBody";
+import SwapHeader from "./SwapHeader";
+import Row, { RowFixed } from "./Row";
 
 const LightDiv = styled.div`
     color: ${colors().text1};
 `
-
+export const Wrapper = styled.div`
+  position: relative;
+  padding: 26px 25px 26px;
+`;
 export default function Swap() {
-    const { useQueryCall, useAuthState } = createReactor({
-        canisterId: import.meta.env.CANISTER_ID_TOKEN1,
-        idlFactory,
-        host: "http://localhost:8080",
-    });
-    
-    const { call, data, loading, error } = useQueryCall({
-        functionName: "icrc1_balance_of",
-        args: [{ owner: "", subaccount: [] }],
-        refetchInterval: 1000,
-        refetchOnMount: true,
-        onLoading: () => console.log("Loading..."),
-        onSuccess: (data) => console.log("Success!", data),
-        onError: (error) => console.log("Error!", error),
-    })
+    // const { useQueryCall, useAuthState } = createReactor({
+    //     canisterId: import.meta.env.CANISTER_ID_TOKEN1,
+    //     idlFactory,
+    //     host: "http://localhost:8080",
+    // });
+
+    // const { call, data, loading, error } = useQueryCall({
+    //     functionName: "icrc1_balance_of",
+    //     args: [{ owner: "", subaccount: [] }],
+    //     refetchInterval: 1000,
+    //     refetchOnMount: true,
+    //     onLoading: () => console.log("Loading..."),
+    //     onSuccess: (data) => console.log("Success!", data),
+    //     onError: (error) => console.log("Error!", error),
+    // })
 
     return (
-        <LightDiv>
-            <h2>ICP Balance:</h2>
-            <LightDiv>
-                Loading: {loading.toString()}
-                <br />
-                Error: {error?.toString()}
-                <br />
-                balance:{" "}
-                {data !== undefined
-                    ? JSON.stringify(data, (_, v) =>
-                        typeof v === "bigint" ? v.toString() : v
-                    )
-                    : null}
-            </LightDiv>
-            <button onClick={call}>Get Balance</button>
-        </LightDiv>
-    )
+        <>
+            <AppBody>
+                <SwapHeader />
+                <Wrapper id='swap-page'>
+                </Wrapper>
+            </AppBody>
+        </>
+    );
 }
