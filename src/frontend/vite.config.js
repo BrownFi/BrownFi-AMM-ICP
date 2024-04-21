45444231bd0211b5a451f39d7ee3938ebab77e64
Truong Ma Phi <trmaphi@gmail.com>
2024-04-21 18:27:10 +0700

    fix: allow styled-component show className with component name

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,18 +1,19 @@
 import { fileURLToPath, URL } from "url";
-import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import environment from "vite-plugin-environment";
 import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import legacy from '@vitejs/plugin-legacy';
 
 /** @type {import('vite').UserConfig} */
 export default ({ mode }) => {
   const rootPath = path.resolve(__dirname, "../../");
-  process.env = { 
-    ...process.env, 
+  process.env = {
+    ...process.env,
     ...loadEnv(mode, rootPath, "CANISTER_"),
-    ...loadEnv(mode, rootPath, "DFX_")
+    ...loadEnv(mode, rootPath, "DFX_"),
   };
-  
+
   return defineConfig({
     build: {
       emptyOutDir: true,
@@ -34,7 +35,19 @@ export default ({ mode }) => {
       },
     },
     plugins: [
-      react(),
+      react({
+        plugins: [
+          [
+            "@swc/plugin-styled-components",
+            {
+              displayName: true,
+              fileName: true,
+              ssr: false,
+            },
+          ],
+        ],
+      }),
+      legacy(),
       environment("all", { prefix: "CANISTER_", defineOn: "import.meta.env" }),
       environment("all", { prefix: "DFX_" }),
     ],
@@ -42,13 +55,16 @@ export default ({ mode }) => {
       alias: [
         {
           find: "declarations",
-          replacement: fileURLToPath(new URL("../declarations", import.meta.url)),
+          replacement: fileURLToPath(
+            new URL("../declarations", import.meta.url)
+          ),
         },
       ],
     },
     define: {
-      'process.env': '{}', // This is to avoid the error 'process is not defined
+      "process.env": "{}", // This is to avoid the error 'process is not defined
       global: "globalThis",
+      "SC_DISABLE_SPEEDY": "true",
     },
-  })
+  });
 };
