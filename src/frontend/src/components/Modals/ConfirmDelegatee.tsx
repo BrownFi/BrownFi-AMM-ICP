5bb51a3370ae145f13224d633294b33dca384c6d
Truong Ma Phi <trmaphi@gmail.com>
2024-06-11 16:29:01 +0700

    fix: no allow click outside to close

--- src/frontend/src/components/Modals/ConfirmDelegatee.tsx
+++ src/frontend/src/components/Modals/ConfirmDelegatee.tsx
@@ -29,20 +29,20 @@ const ConfirmDelegateeModal = (props: ConfirmationModalProps) => {
 	}
 	const useOutsideAlerter = (ref: any) => {
 		useEffect(() => {
-			/**
-			 * Alert if clicked on outside of element
-			 */
-			function handleClickOutside(event: any) {
-				if (ref.current && !ref.current.contains(event.target)) {
-					open(false);
-				}
-			}
-			// Bind the event listener
-			document.addEventListener("mousedown", handleClickOutside);
-			return () => {
-				// Unbind the event listener on clean up
-				document.removeEventListener("mousedown", handleClickOutside);
-			};
+			// /**
+			//  * Alert if clicked on outside of element
+			//  */
+			// function handleClickOutside(event: any) {
+			// 	if (ref.current && !ref.current.contains(event.target)) {
+			// 		open(false);
+			// 	}
+			// }
+			// // Bind the event listener
+			// document.addEventListener("mousedown", handleClickOutside);
+			// return () => {
+			// 	// Unbind the event listener on clean up
+			// 	document.removeEventListener("mousedown", handleClickOutside);
+			// };
 		}, []);
 	};
 
